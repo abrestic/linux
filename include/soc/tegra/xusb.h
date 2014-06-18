d127934ceea5862161215433c83c208c75ebb353
Andrew Bresticker <abrestic@chromium.org>
2014-06-17 17:32:08 -0700

    mailbox: Add NVIDIA Tegra XUSB mailbox driver
    The Tegra xHCI controller's firmware communicates requests to the host
    processor through a mailbox interface.  While there is only a single
    physical channel, messages sent by the controller can be divided
    into two groups: those intended for the PHY driver and those intended
    for the host-controller driver.  The requesting driver is assigned
    one of two virtual channels when the single physical channel is
    requested.  All incoming messages are sent to both virtual channels.
    
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Cc: Jassi Brar <jassisinghbrar@gmail.com>
    ---
    Changes from v6:
     - Access FPCI registers using parent MFD's regmap.
    Changes from v5:
     - Poll for TX completion using MBOX_OWNER field.
    Changes from v4:
     - Use chan->cl to indicate channel allocation status
     - Addressed review comments from Thierry
    No changes from v3.
    Changes from v2:
     - Fixed mailbox IRQ vs. channel alloc/free race.
     - Renamed defines to match TRM.
     - Dropped channel specifier and instead allocated virtual channels as they
       were requested.
     - Removed MODULE_ALIAS.
    Changes from v1:
     - Converted to common mailbox framework.
     - Removed useless polling sequences in TX path.
     - Moved xusb include from linux/ to soc/tegra/

--- include/soc/tegra/xusb.h
+++ include/soc/tegra/xusb.h
@@ -16,4 +16,34 @@ struct tegra_xusb {
 	struct regmap *fpci_regs;
 };
 
+/* Command requests from the firmware */
+enum tegra_xusb_mbox_cmd {
+	MBOX_CMD_MSG_ENABLED = 1,
+	MBOX_CMD_INC_FALC_CLOCK,
+	MBOX_CMD_DEC_FALC_CLOCK,
+	MBOX_CMD_INC_SSPI_CLOCK,
+	MBOX_CMD_DEC_SSPI_CLOCK,
+	MBOX_CMD_SET_BW, /* no ACK/NAK required */
+	MBOX_CMD_SET_SS_PWR_GATING,
+	MBOX_CMD_SET_SS_PWR_UNGATING,
+	MBOX_CMD_SAVE_DFE_CTLE_CTX,
+	MBOX_CMD_AIRPLANE_MODE_ENABLED, /* unused */
+	MBOX_CMD_AIRPLANE_MODE_DISABLED, /* unused */
+	MBOX_CMD_START_HSIC_IDLE,
+	MBOX_CMD_STOP_HSIC_IDLE,
+	MBOX_CMD_DBC_WAKE_STACK, /* unused */
+	MBOX_CMD_HSIC_PRETEND_CONNECT,
+
+	MBOX_CMD_MAX,
+
+	/* Response message to above commands */
+	MBOX_CMD_ACK = 128,
+	MBOX_CMD_NAK
+};
+
+struct tegra_xusb_mbox_msg {
+	u32 cmd;
+	u32 data;
+};
+
 #endif /* __SOC_TEGRA_XUSB_H__ */
